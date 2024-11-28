35a8c5e3662ec07253664dea3ba50369b87d6f19
howard <howard.ueckert@protonmail.com>
2024-11-28 11:24:51 -0600

    set up

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// server.js
+
+// server.js
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+  });
+  
+  
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello There!');
+});
+
+app.listen(3000);
